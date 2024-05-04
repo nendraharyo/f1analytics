46a9cb14d0defd90c687f5c92002c401bd3621da
Nendra Haryo Wijayandanu <haryo.nendra@gmail.com>
2024-05-04 23:25:30 +0700

    add cookies to request

--- scripts/kafkaTestProducer.py
+++ scripts/kafkaTestProducer.py
@@ -1,5 +1,14 @@
+import json
+
 import requests as rq
 from kafka import KafkaProducer
+
+session = rq.session()
+with open(r"data\interim\f1tfeed.json") as file:
+    cookies = json.load(file)[0]  # save them to file as JSON
+    cookies = rq.utils.cookiejar_from_dict(cookies)  # turn dict to cookiejar
+    session.cookies.update(cookies)  # load cookiejar to current session
+
 
 producer = KafkaProducer(bootstrap_servers=["192.168.1.77:9094"])
 print("Harvesting...")
