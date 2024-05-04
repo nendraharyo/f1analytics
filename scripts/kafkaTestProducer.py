65ffbc927a6c1bb8b186946b3e0b003eb5b9e1c1
Nendra Haryo Wijayandanu <haryo.nendra@gmail.com>
2024-05-04 23:26:10 +0700

    remove comments

--- scripts/kafkaTestProducer.py
+++ scripts/kafkaTestProducer.py
@@ -5,9 +5,9 @@ from kafka import KafkaProducer
 
 session = rq.session()
 with open(r"data\interim\f1tfeed.json") as file:
-    cookies = json.load(file)[0]  # save them to file as JSON
-    cookies = rq.utils.cookiejar_from_dict(cookies)  # turn dict to cookiejar
-    session.cookies.update(cookies)  # load cookiejar to current session
+    cookies = json.load(file)[0]
+    cookies = rq.utils.cookiejar_from_dict(cookies)
+    session.cookies.update(cookies)
 
 
 producer = KafkaProducer(bootstrap_servers=["192.168.1.77:9094"])
