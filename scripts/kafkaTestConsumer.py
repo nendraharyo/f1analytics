9fe4db599647fac7fadaf13899eac5d138e484fc
Nendra Haryo Wijayandanu <haryo.nendra@gmail.com>
2024-05-05 15:34:42 +0700

    change Topic name, cookie fix

--- scripts/kafkaTestConsumer.py
+++ scripts/kafkaTestConsumer.py
@@ -1,11 +1,10 @@
 from kafka import KafkaConsumer
 
+# from f1analytics.tools import converter
+
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer("quickstart-events", bootstrap_servers=["192.168.1.77:9094"])
+consumer = KafkaConsumer("f1tfeed_raw", bootstrap_servers=["192.168.1.77:9094"])
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print(
-        "%s:%d:%d: key=%s value=%s"
-        % (message.topic, message.partition, message.offset, message.key, message.value)
-    )
+    print(message.value)
--- scripts/kafkaTestProducer.py
+++ scripts/kafkaTestProducer.py
@@ -5,20 +5,42 @@ from kafka import KafkaProducer
 
 session = rq.session()
 with open(r"data\interim\f1tfeed.json") as file:
-    cookies = json.load(file)[0]
-    cookies = rq.utils.cookiejar_from_dict(cookies)
-    session.cookies.update(cookies)
+    cookies = json.load(file)
+
+cookie_props = [
+    "name",
+    "value",
+    "port",
+    "domain",
+    "path",
+    "expires",
+    "secure",
+    "discard",
+]
+cookiejar = [
+    dict(
+        [
+            (k, item["session" if k == "discard" else k])
+            for k in cookie_props
+            if k in item
+        ]
+    )
+    for item in cookies
+]
+
+for c in cookiejar:
+    session.cookies.set(**c)
 
 producer = KafkaProducer(bootstrap_servers=["192.168.1.77:9094"])
 print("Harvesting...")
 while True:
     data = (
-        session.get(r"https://f1.tfeed.net/tt.js")
+        session.get(url=r"https://f1.tfeed.net/tt.js")
         .text.replace("\n", "")
         .replace("\t", "")
         .replace("\r", "")
         .replace("//", "")
     )
     if "ntt_f" in data:
-        producer.send("quickstart-events", data.encode())
+        producer.send("f1tfeed_raw", data.encode())
 producer.flush()
