c8c026f22c15e4c4ae3b507567e11fbc62ad635b
Nendra Haryo Wijayandanu <haryo.nendra@gmail.com>
2024-05-16 20:15:59 +0700

    add connection to db and push all data

--- scripts/kafkaTestConsumer.py
+++ scripts/kafkaTestConsumer.py
@@ -1,10 +1,28 @@
-from kafka import KafkaConsumer
+import os
 
-# from f1analytics.tools import converter
+import pandas as pd
+from kafka import KafkaConsumer
+from sqlalchemy import create_engine
+
+from f1analytics.tools import converter
+
+engine = create_engine(os.environ.get("stbdb_url"))
+conn = engine.connect()
 
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer("f1tfeed_raw", bootstrap_servers=["192.168.1.77:9094"])
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print(message.value)
+    # print(message.value)
+    tlmy_df, weather_df, race_df = converter.convert_ntt(message.value)
+    tlmy_df["timestamp"] = pd.to_datetime(tlmy_df["timestamp"], unit="ms")
+    weather_df["timestamp"] = pd.to_datetime(weather_df["timestamp"], unit="ms")
+    race_df["timestamp"] = pd.to_datetime(race_df["timestamp"], unit="ms")
+    tlmy_df["throttle"] = tlmy_df["throttle"].astype(int)
+    tlmy_df = tlmy_df.set_index("timestamp")
+    weather_df_df = weather_df.set_index("timestamp")
+    race_df = race_df.set_index("timestamp")
+    tlmy_df.to_sql("telemetry_imola_2024", conn, if_exists="append")
+    weather_df.to_sql("weather_imola_2024", conn, if_exists="append")
+    race_df.to_sql("racestats_imola_2024", conn, if_exists="append")
