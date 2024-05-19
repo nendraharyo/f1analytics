1ab3c1909f473409b325202ba7d73a881bbf0d4a
Nendra Haryo Wijayandanu <haryo.nendra@gmail.com>
2024-05-19 19:08:28 +0700

    add break signal if chequered flag is out

--- scripts/kafkaTestConsumer.py
+++ scripts/kafkaTestConsumer.py
@@ -11,18 +11,26 @@ conn = engine.connect()
 
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer("f1tfeed_raw", bootstrap_servers=["192.168.1.77:9094"])
+print("Mulai memproses data..")
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
     # print(message.value)
-    tlmy_df, weather_df, race_df = converter.convert_ntt(message.value)
-    tlmy_df["timestamp"] = pd.to_datetime(tlmy_df["timestamp"], unit="ms")
-    weather_df["timestamp"] = pd.to_datetime(weather_df["timestamp"], unit="ms")
-    race_df["timestamp"] = pd.to_datetime(race_df["timestamp"], unit="ms")
-    tlmy_df["throttle"] = tlmy_df["throttle"].astype(int)
-    tlmy_df = tlmy_df.set_index("timestamp")
-    weather_df_df = weather_df.set_index("timestamp")
-    race_df = race_df.set_index("timestamp")
-    tlmy_df.to_sql("telemetry_imola_2024", conn, if_exists="append")
-    weather_df.to_sql("weather_imola_2024", conn, if_exists="append")
-    race_df.to_sql("racestats_imola_2024", conn, if_exists="append")
+    try:
+        tlmy_df, weather_df, race_df = converter.convert_ntt(message.value.decode())
+        if race_df["flag_status"] != "6":
+            tlmy_df["timestamp"] = pd.to_datetime(tlmy_df["timestamp"], unit="ms")
+            weather_df["timestamp"] = pd.to_datetime(weather_df["timestamp"], unit="ms")
+            race_df["timestamp"] = pd.to_datetime(race_df["timestamp"], unit="ms")
+            tlmy_df["throttle"] = tlmy_df["throttle"].astype(int)
+            tlmy_df = tlmy_df.set_index("timestamp")
+            weather_df_df = weather_df.set_index("timestamp")
+            race_df = race_df.set_index("timestamp")
+            tlmy_df.to_sql("telemetry_imola_race_2024", conn, if_exists="append")
+            weather_df.to_sql("weather_imola_race_2024", conn, if_exists="append")
+            race_df.to_sql("racestats_imola_race_2024", conn, if_exists="append")
+        else:
+            print("sudah ada sinyal checkered flag")
+            break
+    except:
+        continue
